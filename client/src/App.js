3f9adc2f1b8712d7e070382263e0be15b8ffb902
jeremychow <jmrchow@gmail.com>
2021-11-19 09:37:31 -0500

    delete works merge with trevors bs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Markers from './components/markers/marker'
+import Dictaphone from './components/dictaphone';
+import Player from './components/players/player';
 import LoopList from './components/LoopList.js'
 import {Row, Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,6 +9,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>      
+      <Player/>
+     
       <Row>
         <Col>
           <LoopList></LoopList>
---
+++ client/src/components/dictaphone.js
@@ -0,0 +1,104 @@
+//import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useState, useEffect } from 'react'
+
+
+function Dictaphone(props) {
+
+  const [message, setMessage] = useState({
+    request: '',
+    name: '',
+    firstTimeStamp: '',
+    secondTimeStamp: ''
+  })
+
+  const commands = [
+
+    //Add marker and loop with name
+    {
+      command: 'add marker called :name at :timestamp',
+      callback: (name, timestamp) => setMessage({request: 'addMarker', name: name, firstTimeStamp: timestamp})
+    },
+    {
+      command: 'Add loop called :name from :firstTimeStamp to :secondTimeStamp ',
+      callback: (name, firstTimeStamp, secondTimeStamp) => 
+          setMessage({request: 'addLoop', name: name, firstTimeStamp: firstTimeStamp, secondTimeStamp: secondTimeStamp})
+    },
+    //Add marker and loop without name
+    {
+      command: 'add marker at :timestamp',
+      callback: (timestamp) => setMessage({request: 'addMarker', firstTimeStamp: timestamp})
+    },
+    {
+      command: 'Add loop from :firstTimeStamp to :secondTimeStamp ',
+      callback: (firstTimeStamp, secondTimeStamp) => 
+          setMessage({request: 'addLoop', firstTimeStamp: firstTimeStamp, secondTimeStamp: secondTimeStamp})
+    },
+    //Delete marker and loop 
+    {
+      command: 'Delete marker called :name ',
+      callback: (name) => 
+          setMessage({request: 'delMarker', name: name})
+    },
+    {
+      command: 'Delete loop called :name ',
+      callback: (name) => 
+          setMessage({request: 'delLoop', name: name})
+    },
+    {
+      command: 'skip ahead',
+      callback: () =>
+          setMessage({request: 'skipFwd'})
+    },
+    {
+      command: 'skip backwards',
+      callback: () =>
+          setMessage({request: 'skipBwd'})
+    },
+    {
+      command: 'restart',
+      callback: () =>
+          setMessage({request: 'restart'})
+    }
+
+  ]
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition({commands});
+  
+  
+  function handleTranscriptChange(event){
+    console.log(event.target.value)
+  }
+
+  // Everytime message is updated, reset it and send data to parent
+  useEffect(() => {
+    props.sendToPlayer(message)
+    resetTranscript()
+
+    return () => {
+      resetTranscript()
+    }
+  }, [message])
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{message.request + " " + message.name}</p>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
